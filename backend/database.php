f150fbc11c04918ddcd179d4ea14afb019026138
bananalden <128482606+bananalden@users.noreply.github.com>
2024-05-08 13:28:16 +0800

    connected to database, search for books works

--- backend/database.php
+++ backend/database.php
@@ -8,12 +8,5 @@
 
  $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
- if(!$conn) {
-    die("Connection Failed: ". mysqli_connect_error());
- }
-
- else{
-    echo "connected!";
- }
 
 ?>
