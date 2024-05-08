c58577915342db97fe453bfc7dfdd82af1b450a1
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2024-05-08 15:56:57 +0800

    front-end updated

--- index.php
+++ index.php
@@ -4,28 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="">
+    <link href="https://fonts.googleapis.com/css2?family=Briem+Hand:wght@100..900&family=Sedan+SC&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css folder/index.css">
 
 </head>
 <body>
     
-    <form  action="backend/logcheck.php" method="post">  
-        <div class="container-login">  
-            <div class="title">
-                <h1>LIBRARIAN LOGIN FORM</h1>   
-            </div>
-            
-            <div class="user-details"> 
-                <input type="text" placeholder="Enter Username" name="uname" required>
-            </div>
+    <form class="container" action="backend/logcheck.php" method="post">  
 
-            <div class="user-details">  
-                <input type="password" placeholder="Enter Password" name="pword" required>
-            </div>  
-            <button type="submit" name="submit">Login</button>
+        <div class="container-login">
+            <h1>Librarian Form</h1>
         </div>
 
-       
+        <div class="user-details">
+            <div class="user-container">
+                <input type="text" class="input-box" placeholder="Enter Username">
+            </div>
+
+            <div class="user-container">
+                <input type="password" class="input-box" placeholder="Enter Password">
+            </div>
+        </div>
+
+        <div class="d-flex align-items-center justify-content-center">
+            <div class="form-check w-25 mb-0">
+                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                <label class="form-check-label" for="form2Example3">
+                    Remember me
+                </label>
+            </div>
+            <p href="#!" class="text-light">Forgot password?</p>
+        </div>
+
+        <p id="register" class="small fw-bold mt-2 pt-1 mb-0 text-light">Don't have an account? <a href="#!"
+                class="link-dange" style="color:#e85c29">Register</a></p>
+
+        <div class="button">
+            <button class="custom-button">Sign in</button>
+        </div>
+
+
     </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
