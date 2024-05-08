2037e03c8cacab00e951713f82a4b00ce52c8699
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2024-05-08 17:08:46 +0800

    front-end updated 1

---
+++ registration.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Page</title>
+    <link href="https://fonts.googleapis.com/css2?family=Briem+Hand:wght@100..900&family=Sedan+SC&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css folder/registration.css">
+
+</head>
+<body>
+    
+    <form class="container" action="backend/logcheck.php" method="post">  
+
+        <div class="container-login">
+            <h1>User Registration Form</h1>
+        </div>
+
+        <div class="user-details">
+            <div class="user-container">
+                <input type="text" class="input-box" placeholder="First Name">
+            </div>
+
+            <div class="user-container">
+                <input type="text" class="input-box" placeholder="Last Name">
+            </div>
+
+            <div class="user-container">
+                <input type="text" class="input-box" placeholder="User Name">
+            </div>
+
+            <div class="user-container">
+                <input type="password" class="input-box" placeholder="Password">
+            </div>
+
+            <div class="user-container">
+                <input type="text" class="input-box" placeholder="Roll No">
+            </div>
+
+            <div class="user-container">
+                <input type="text" class="input-box" placeholder="Email">
+            </div>
+        </div>
+
+
+        <div class="button">
+            <button class="custom-button">Sign in</button>
+        </div>
+
+
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
