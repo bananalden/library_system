237efe418590ac91d19cab5696a44c0093b5438a
bananalden <128482606+bananalden@users.noreply.github.com>
2024-05-09 16:37:31 +0800

    reformatted document for readability

--- backend/insertbook.php
+++ backend/insertbook.php
@@ -14,42 +14,25 @@ if (isset($_POST["insertdata"])) {
     $curYear = date('Y');
 
 
-  
-        if (!validateNumberDigits($isbn, $digitforISBN)){
-            header('location: ../booklisting.php?error=0');
-            exit();
+
+    if (!validateNumberDigits($isbn, $digitforISBN)) {
+        header('location: ../booklisting.php?error=0');
+        exit();
+    } else if (!validateNumberDigits($yearpublished, $digitforYear)) {
+        header('location: ../booklisting.php?error=1');
+        exit();
+    } else {
+
+
+        $sql = "INSERT INTO booklist (isbn, bookname, author, category, yearpublished) VALUES ('$isbn', '$title', '$author', '$category','$yearpublished')";
+
+        if ($conn->query($sql) === TRUE) {
+            header('location: ../booklisting.php?error=3');
+        } else {
+            header('location: ../booklisting.php?error=4');
         }
-
-        else if(!validateNumberDigits($yearpublished, $digitforYear))
-            {
-                header('location: ../booklisting.php?error=1');
-                exit();
-            }
-        
-        else{
-            
-
-            $sql = "INSERT INTO booklist (isbn, bookname, author, category, yearpublished) VALUES ('$isbn', '$title', '$author', '$category','$yearpublished')";
-
-            if ($conn->query($sql) === TRUE) {
-                header('location: ../booklisting.php?error=3');
-            } else {
-                header('location: ../booklisting.php?error=4');
-            }
-            
-
-
-        }
-
-
-
-            
-        }
-        
-    
-
-
-else {
+    }
+} else {
 
     header('location: ../booklisting.php');
 }
