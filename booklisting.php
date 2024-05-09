e8fe726e994546fb0be5b2a1084d51d7ea248b60
bananalden <128482606+bananalden@users.noreply.github.com>
2024-05-09 10:29:11 +0800

    Fixed Form Link

--- booklisting.php
+++ booklisting.php
@@ -2,135 +2,155 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Library System</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" />
 </head>
 
 <body>
-    <!-- =================NAVBAR SECTION ================-->
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-        <div class="container">
-            <a href="mainpage.php" class="navbar-brand">ACLC Library System</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a href="mainpage.php" class="nav-link">Home Page</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active">Book list</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">User List</a>
-                    </li>
-                </ul>
-                <a class="nav-item mr-3 nav-link p-3 text-light" href="#" style="background-color: #e85c29">Logout</a>
-    </nav>
-    </div>
-    </div>
-    </nav>
-     <!-- =================NAVBAR SECTION END================-->
-    <section class="bg-light text-dark p-5 text-left">
-        <div class="container">
-            <div class="d-flex">
-                <div>
-                    <!-- ========== Search Form Start ========== -->
-                    <h1>Book Listings</h1>
-                    <div>
-                    <button type="button" class="btn btn-primary float-right" data-bs-toggle="modal" data-bs-target="#insertmodal">Enter new Book</button>
-                    </div>
-                    <!-- ========== Search Form End ========== -->
-
-
-
-
-                    <!-- ========== Table Section Start ========== -->
-                    <div class="col-md-12 p-20">
-                        <div class="container">
-                        
-                           
-                            <table id="myTable" class="table table-bordered">
-                            <thead class="thead-dark">
-                                    <tr>
-                                        <th scope="col">ISBN</th>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Author</th>
-                                        <th scope="col">Category</th>
-                                        <th scope="col">Year Published</th>
-                                    </tr>
-                            </thead>
-                                <tbody>
-                                <?php 
-                            require('backend/database.php'); 
-                            $query = "SELECT * FROM booklist";
-                            $run_query = mysqli_query($conn, $query);
-
-                            if(mysqli_num_rows($run_query) > 0)
-                            {
-                                foreach($run_query as $row)
-                                {
-                                    ?>
-
-                                    <tr>
-                                        <th scope="row"><?=$row['isbn'] ?></th>
-                                        <td><?=$row['bookname'] ?></td>
-                                        <td><?=$row['author'] ?></td>
-                                        <td><?=$row['category'] ?></td>
-                                        <td><?=$row['yearpublished'] ?></td>
-                                    </tr>
-                    <?php
-
-                                }
-                            }
-                            
-                            else
-                            {
-
-                            }
-                                  ?> 
-                                </tbody> 
-                            </table>
-                        </div>
-                    </div>
-                    <!-- ========== Table Section End ========== -->
-
-                
-                    </div>
-                    
-                    <div class="modal fade" id="insertmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        INSERT FORM HERE
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
+  <!-- =================NAVBAR SECTION ================-->
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div class="container">
+      <a href="mainpage.php" class="navbar-brand">ACLC Library System</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="mainpage.php" class="nav-link">Home Page</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link active">Book list</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">User List</a>
+          </li>
+        </ul>
+        <a class="nav-item mr-3 nav-link p-3 text-light" href="#" style="background-color: #e85c29">Logout</a>
+  </nav>
   </div>
-</div>
+  </div>
+  </nav>
+  <!-- =================NAVBAR SECTION END================-->
+  <section class="bg-light text-dark p-5 text-left">
+    <div class="container">
+      <div class="d-flex">
+        <div>
+          <!-- ========== Search Form Start ========== -->
+          <h1>Book Listings</h1>
+          <div>
+            <button type="button" class="btn btn-primary float-right" data-bs-toggle="modal" data-bs-target="#insertmodal">Enter new Book</button>
+          </div>
+          <!-- ========== Search Form End ========== -->
+
+
+
+
+          <!-- ========== Table Section Start ========== -->
+          <div class="col-md-12 p-20">
+            <div class="container">
+
+
+              <table id="myTable" class="table table-bordered">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">ISBN</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Year Published</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  require('backend/database.php');
+                  $query = "SELECT * FROM booklist";
+                  $run_query = mysqli_query($conn, $query);
+
+                  if (mysqli_num_rows($run_query) > 0) {
+                    foreach ($run_query as $row) {
+                  ?>
+
+                      <tr>
+                        <th scope="row"><?= $row['isbn'] ?></th>
+                        <td><?= $row['bookname'] ?></td>
+                        <td><?= $row['author'] ?></td>
+                        <td><?= $row['category'] ?></td>
+                        <td><?= $row['yearpublished'] ?></td>
+                      </tr>
+                  <?php
+
+                    }
+                  }
+                  ?>
+                </tbody>
+              </table>
             </div>
+          </div>
+          <!-- ========== Table Section End ========== -->
+
+
         </div>
-    </section>
-    <!-- ######   SCRIPTS HERE     -######-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-    <script>
-  $(document).ready( function () {
-    $('#myTable').DataTable();
-  });
-</script>
+
+        <div class="modal fade" id="insertmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Insert Book Details Here</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+
+            <div class="modal-body">
+            <form action="backend/insertbook.php" method="post">
+                  <div class="form-group">
+                    <label>ISBN</label>
+                    <input type="text" name="isbn" class="form-control"></input>
+                  </div>
+                  <div class="form-group">
+                    <label>Title</label>
+                    <input type="text" name="title" class="form-control"></input>
+                  </div>
+                  <div class="form-group">
+                    <label>Author</label>
+                    <input type="text" name="author" class="form-control"></input>
+                  </div>
+                  <div class="form-group">
+                    <label>Category</label>
+                    <select name="category" class="form-control">
+                        <option value="Fiction">Fiction</option>
+                        <option value="Non-Fiction">Non-Fiction</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label>Year Published</label>
+                    <input type="number" name="yearpublished" class="form-control"></input>
+                  </div>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" name="insertdata" class="btn btn-primary">Save changes</button>
+              </div>
+            </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ######   SCRIPTS HERE     -######-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+  <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#myTable').DataTable();
+    });
+  </script>
 </body>
 
 </html>
