fee0a018244aa99c8f55be18cd95adb30087479c
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2024-05-10 15:36:19 +0800

    front-end new 6

--- mainpage.php
+++ mainpage.php
@@ -53,10 +53,11 @@
     </section>
 
     <section id="section-2">
-        <div class="about-us">
-        <h1>About us</h1>
+        <div id="title">
+            <h1>About Us</h1>
         </div>
     </section>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
