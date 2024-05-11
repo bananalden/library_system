54698422e1ef4d0394dd502bd7ea234a1a0763bd
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2024-05-11 14:14:11 +0800

    frond-end updated new 7

--- mainpage.php
+++ mainpage.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css folder/mainpage.css">
 </head>
 
@@ -33,12 +35,13 @@
         </div> 
     </nav>
     </nav>
+
     <section id="section-1" class="text-dark p-5 text-left">
         <div class="container">
            <div class="container">
                 <div class="row">
                     <div class="col">
-                    <img id="img" src="./image/library 1.jpeg" class="" alt="...">
+                        <img id="img" src="./image/library 1.jpeg" class="" alt="...">
                     </div>
                     <div id="quotes" class="col">
                         <p>"Libraries store the energy that fuels the imagination. They open up windows to the world and inspire us to explore and achieve, and contribute to improving our quality of life."
@@ -47,18 +50,65 @@
                         - Sidney Sheldon</p>
                     </div>
                 </div>
-           </div>
             </div>
         </div>
     </section>
 
-    <section id="section-2">
-        <div id="title">
-            <h1>About Us</h1>
+
+    <section id="section-2" class="text-dark p-5 text-left">
+        <h1>About Us</h1>
+        <div class="container">
+           <div class="container">
+                <div class="row">
+                    <div class="row-1 col">
+                        <img id="img-4" src="./image/books 4.jpeg" class="" alt="img 4">
+
+                        <div class="col">
+                            <img id="img-8" src="./image/books 8.jpeg" class="" alt="img 8">
+                        </div>
+                    </div>
+
+                    <div id="row-2" class="col">
+                        <img id="img-7" src="./image/books 7.jpeg" class="" alt="img 7">
+
+                        <div class="col">
+                            <img id="img-6" src="./image/books 6.jpeg" class="" alt="img 6">
+                        </div>
+                    </div>
+
+                    <div id="about-us" class="col">
+                        <p>Welcome to ACLC Library System
+                            <br>
+                            <br>
+                            At ACLC Library System, we believe in the power of knowledge and the importance of access to information for all. Our library system is dedicated to serving our community by providing a diverse range of resources, programs, and services that promote lifelong learning, literacy, and cultural enrichment.
+                            <br>
+                            <br>
+                            Our Mission
+                            <br>
+                            <br>
+                            Our mission is to connect people with the information they need to learn, grow, and thrive. We strive to be a welcoming and inclusive space where individuals of all ages and backgrounds can explore, discover, and engage with the world around them.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
+
+
+    <section id="section-3"></section>
+
+
+
+
+
+
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
+
+<!-- width="200" height="200  class="img-thumbnail"" -->
