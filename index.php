36662f47a461bc6c80384f3428d63b5bd9c1be90
bananalden <128482606+bananalden@users.noreply.github.com>
2024-05-13 17:36:52 +0800

    error on text

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Library System</title>
     <link href="https://fonts.googleapis.com/css2?family=Briem+Hand:wght@100..900&family=Sedan+SC&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css folder/index.css">
+    <link rel="stylesheet" href="./css_folder/login.css">
 
 </head>
 <body>
