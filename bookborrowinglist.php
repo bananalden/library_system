92232e174a1240239aba1710a8f8c281361a52d1
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2024-05-13 10:45:15 +0800

    front-end new

--- bookborrowinglist.php
+++ bookborrowinglist.php
@@ -13,13 +13,14 @@ require 'backend/adminauthcheck.php';
   <title>Book Return</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" />
+  <link rel="stylesheet" href="./css folder/borrowlist.css">
 </head>
 
 <body>
   <!-- =================NAVBAR SECTION ================-->
   <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container">
-      <a href="mainpage.php" class="navbar-brand">ACLC Library System</a>
+      <a href="mainpage.php" class="title navbar-brand">ACLC Library System</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"><span class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse" id="navbar">
         <ul class="navbar-nav ms-auto">
