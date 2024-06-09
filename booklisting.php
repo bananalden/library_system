c059d545d9d2130185e0a397e477878e1ccbc1a6
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2024-06-09 10:46:11 +0800

    UPDATED 4

--- booklisting.php
+++ booklisting.php
@@ -19,7 +19,7 @@ error_reporting(E_ERROR | E_PARSE);
 <div class="sidebar">
         <ul>
         <li><a href="mainpage.php">Home</a></li>
-          <li><a href="#">Book List</a></li>
+          <li><a href="booklisting.php">Book List</a></li>
           <li><a href="recyclebin.php">Trash</a></li>
           <li><a href="userlist.php">Admin List</a></li>
           <li><a href="studentlist.php">Student List</a></li>
